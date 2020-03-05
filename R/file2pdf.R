e39c6ea95d6031d05ea1beb6e304f0c4c0aaf979
Keon-Woong Moon <cardiomoon@gmail.com>
2020-03-05 20:50:48 +0900

    0.2.0
    0.2.0

--- R/file2pdf.R
+++ R/file2pdf.R
@@ -49,6 +49,23 @@ file2pptx=function(file,selected=NULL,...){
     data2pptx(data=data,preprocessing=preprocessing,...)
 }
 
+#' read data file and make a pptx file with Rmd file
+#' @param file The name of the file which the data are to be read from.
+#' @param selected A numeric vector or NULL(default). If specified, only selected data are printed.
+#' @param ... Further argument to be passed to data2pptx()
+#' @export
+file2pptx2=function(file,selected=NULL,...){
+    data=readCSVComment(file)
+    preprocessing=readComment(file)
+
+    if(!is.null(selected)){
+        count=nrow(data)
+        accept=which((selected>0) & (selected<=count))
+        data<-data[selected[accept],]
+    }
+    data2pptx2(data=data,preprocessing=preprocessing,...)
+}
+
 #' read data file and make a docx file
 #' @param file The name of the file which the data are to be read from.
 #' @param selected A numeric vector or NULL(default). If specified, only selected data are printed.
@@ -64,6 +81,23 @@ file2docx=function(file,selected=NULL,...){
         data<-data[selected[accept],]
     }
     data2docx(data=data,preprocessing=preprocessing,...)
+}
+
+#' read data file and make a docx file with Rmd file
+#' @param file The name of the file which the data are to be read from.
+#' @param selected A numeric vector or NULL(default). If specified, only selected data are printed.
+#' @param ... Further argument to be passed to data2docx()
+#' @export
+file2docx2=function(file,selected=NULL,...){
+    data=readCSVComment(file)
+    preprocessing=readComment(file)
+
+    if(!is.null(selected)){
+        count=nrow(data)
+        accept=which((selected>0) & (selected<=count))
+        data<-data[selected[accept],]
+    }
+    data2docx2(data=data,preprocessing=preprocessing,...)
 }
 
 #' read data file and make a zip file with plots
